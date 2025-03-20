b64f0a454eea9f0a34870049cb329532a892d0d5
Yashraj Deshmukh <yashrajdeshmukh@eng.rizvi.edu.in>
2025-03-20 18:26:21 +0530

    api for bot query added

--- controllers/BotController.js
+++ controllers/BotController.js
@@ -1,3 +1,12 @@
-export async function GenerateResponse(){
-    
+import { ReplyUser } from "../lib/gemini.js";
+
+export async function GenerateResponse(req,res){
+    const {query} = req.body;
+    try{
+        const reply = await ReplyUser(query)
+        res.status(200).json({botReply:reply})
+    }catch(err){
+        console.log(err);
+        res.status(500).json({message:"Internal server error!"})
+    }
 }
